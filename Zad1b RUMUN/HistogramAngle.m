d2643a28ddf98be4a452a670b7196414d3cda359
sqodo <sqodo48@github.com>
2020-04-25 22:08:22 +0200

    Zadanie 1b - wersja z komentarzami
    Wymaga poprawek, opisane w TODO

--- Zad1b RUMUN/HistogramAngle.m
+++ Zad1b RUMUN/HistogramAngle.m
@@ -1,34 +1,37 @@
+%TODO: ustawić w coppelli wszystkie zasięgi czujniów tak samo - sterować
+%bąblem w matlabie
+
 %Obliczanie wektora przyciągającego do celu
 function [angle,d_hist]=HistogramAngle(detStateV, detPointM, v_robot, Ki, delta_t, angle_error)
-bubble_boundary=v_robot*delta_t*Ki; %ustal granicę detekcji
+bubble_boundary=v_robot*delta_t*Ki; %ustal granicę detekcji %to do: zastąpić skalarny bąbel wektorem dla każdego czujnika osobno
 angle_sum=0;
 d_sum=0;
 d=zeros(8,1);
 sensor_angles=[90;67.5;45;22.5;-22.5;-45;-67.5;-90];
-%sensor_angles=[90;50;30;10;-10;-30;-50;-90];
+
 for i=1:8
 
-   if detStateV(i) == 1
-      dist=CalcDist([0;0],[detPointM(i,1);detPointM(i,3)]);
+   if detStateV(i) == 1  %%czy i-ty czujnik widzi przeszkodę
+      dist=CalcDist([0;0],[detPointM(i,1);detPointM(i,3)]);  %oblicz odległość od i-tego czujnika do wykrytej przeszkody
       
       if dist> bubble_boundary
-          d(i)=1;
+          d(i)=1; %wykryto przeszkodę, ale jest dalej niż bąbel
       else
-       %d(i)=dist/bubble_boundary;
-       d(i)=dist;
+          d(i)=dist; %wykryto przeszkodę w odległości mniejszej od bąbla
       end
    else
-       d(i)=1;
+       d(i)=1; %nie wykryto przeszkody
    end
    
-    angle_sum=angle_sum+sensor_angles(i)*d(i);
+    angle_sum=angle_sum+sensor_angles(i)*d(i); %TODO: sprawdzić czy kąty są wagami
     d_sum=d_sum+d(i);
 end
 
-
+%robot jedzie prosto na przeszkodę - dwa środkowe sensory wykryły obecność
 if (detStateV(4)==1 && detStateV(5)==1 && detStateV(1)==0 && detStateV(2)==0 && detStateV(3)==0 && detStateV(6)==0 && detStateV(7)==0 && detStateV(8)==0)
 
-angle=2*(angle_sum/d_sum)+sign(angle_error)*15;
+angle=2*(angle_sum/d_sum)+sign(angle_error)*10; %TODO: sprawdzić mnożnik 
+                                                  % zastanowić się jak zmieniać kierunek jadąc wprost na ścianę             
 
 else
    angle=2*(angle_sum/d_sum); 
--- Zad1b RUMUN/simpleTest.m
+++ Zad1b RUMUN/simpleTest.m
@@ -1,65 +1,33 @@
-% Make sure to have the server side running in CoppeliaSim: 
-% in a child script of a CoppeliaSim scene, add following command
-% to be executed just once, at simulation start:
-%
-% simRemoteApi.start(19999)
-%
-% then start simulation, and run this program.
-%
-% IMPORTANT: for each successful call to simxStart, there
-% should be a corresponding call to simxFinish at the end!
-
 
 
 function simpleTest()
-    r_pose=[0 0 0];
+
+
     disp('Program started');
-    % sim=remApi('remoteApi','extApi.h'); % using the header (requires a compiler)
+    
     sim=remApi('remoteApi'); % using the prototype file (remoteApiProto.m)
     sim.simxFinish(-1); % just in case, close all opened connections
     clientID=sim.simxStart('127.0.0.1',19999,true,true,5000,5);
 
     if (clientID>-1)
         disp('Connected to remote API server');
-            
-        % Now try to retrieve data in a blocking fashion (i.e. a service call):
-%         [res,objs]=sim.simxGetObjects(clientID,sim.sim_handle_all,sim.simx_opmode_blocking);
-%         if (res==sim.simx_return_ok)
-%             fprintf('Number of objects in the scene: %d\n',length(objs));
-%         else
-%             fprintf('Remote API function call returned with error code: %d\n',res);
-%         end
-        
-        
-        startLocation = [];
+         
+        %inicjalizacja zmiennych
+        r_pose=[0 0 0]; 
         baseLocation=[5 5];
         sensorLocation=[1 9; 9 9; 9.25 2.25; 2.5 3.5; 6.5 2.0];
         sensorID=randperm(5,1); %wylosuj czujnik 
-        %dist=CalcDist(startLocation,startLocation);
-        dist=0;
-        dist2base=0;
-
+        goal=[5; 5]; %to musi być wektor pionowy! 
         perform_search=1;
-        path_currentpoint=1;
         path_completed=0;
-        %show(prmSimple)
         angle_error=0;
-        
+        d=ones(8,1); %inicjalizacja histogramu
+        %reorient=0;
+%         angle_hist=[0];
+%         phi_hist=[0];
+%         alpha_hist=[0];
         pause(2);
-    
-        % Now retrieve streaming data (i.e. in a non-blocking fashion):
-        %t=clock;
-        %startTime=t(6);
-        %currentTime=t(6);
-        %sim.simxGetIntegerParameter(clientID,sim.sim_intparam_mouse_x,sim.simx_opmode_streaming); % Initialize streaming
-        %while (currentTime-startTime < 5)   
-        %    [returnCode,data]=sim.simxGetIntegerParameter(clientID,sim.sim_intparam_mouse_x,sim.simx_opmode_buffer); % Try to retrieve the streamed data
-        %    if (returnCode==sim.simx_return_ok) % After initialization of streaming, it will take a few ms before the first value arrives, so check the return code
-        %        fprintf('Mouse position x: %d\n',data); % Mouse position x is actualized when the cursor is over CoppeliaSim's window
-        %    end
-        %    t=clock;
-        %    currentTime=t(6);
-        %end
+
         [returnCode, left_Motor]=sim.simxGetObjectHandle(clientID,'Pioneer_p3dx_leftMotor', sim.simx_opmode_blocking);
         [returnCode, right_Motor]=sim.simxGetObjectHandle(clientID,'Pioneer_p3dx_rightMotor', sim.simx_opmode_blocking);
         [returnCode, ultrasonicSensor1]=sim.simxGetObjectHandle(clientID,'Pioneer_p3dx_ultrasonicSensor1', sim.simx_opmode_blocking);
@@ -72,30 +40,20 @@ function simpleTest()
         [returnCode, ultrasonicSensor8]=sim.simxGetObjectHandle(clientID,'Pioneer_p3dx_ultrasonicSensor8', sim.simx_opmode_blocking);
         [returnCode, handle]= sim.simxGetObjectHandle(clientID,'Pioneer_p3dx', sim.simx_opmode_blocking);
         handle;
-                
-         %for i=1:50
-         while 2 > 1      	
+          
+         while 2 > 1      	%GŁÓWNA PĘTLA SYMULACJI
                 
 
-                %if detectionState == 1
-%              detectedPointLeft;
-%              XLobstacle=(detectedPointLeft(3)*cosd(10)+0.2)*double(detectionStateLeft);
-%              YLobstacle=(detectedPointLeft(1)*cosd(10)+0.02)*double(detectionStateLeft);
-%              XRobstacle=(detectedPointRight(3)*cosd(10)+0.2)*double(detectionStateRight);
-%              YRobstacle=(detectedPointRight(1)*cosd(10)-0.02)*double(detectionStateRight);
-%              Xobstacle=double(1/(detectionStateLeft+detectionStateRight))*(double(XLobstacle+XRobstacle))
-%              Yobstacle=double(1/(detectionStateLeft+detectionStateRight))*(double(YLobstacle+YRobstacle))
-%              distance=sqrt(double(Xobstacle^2+Yobstacle^2));
-%              rad2deg(atan2(double(Yobstacle),double(Xobstacle)));
-             %end
-             
+
+                    %pobierz z Copelli położenie robota, kąt i prędkość
                 [returnCode, position]=sim.simxGetObjectPosition(clientID,16 , -1, sim.simx_opmode_blocking);
                 [returnCode, orientation]=sim.simxGetObjectOrientation(clientID,16 , -1, sim.simx_opmode_blocking);
                 [returnCode,linearVelocity,rotationVel]=sim.simxGetObjectVelocity(clientID,16,sim.simx_opmode_blocking);         
-                r_pose=position+[5, 5, 0];
-                alfa_abs=orientation(3);
+                r_pose=position+[5, 5, 0]; %przesunięcie układu współrzędnych 
+                alfa_abs=orientation(3); %orientacja robota (-pi;pi)
+              
                 
-                goal=[1;1];
+                %zmniejsz prędkość gdy robot jest już blisko celu
                 if CalcDist(r_pose,goal) > 0.3
                 v_des=0.1;
                 else
@@ -103,7 +61,7 @@ function simpleTest()
                 end
                 
 
-                
+                %pobierz dane z Copelli dot. czujników zbliżeniowych
                 
                 [returnCode,detectionState_1, detectedPoint_1,detectedObjectHandle,detectedSurfaceNormalVector]=sim.simxReadProximitySensor(clientID,ultrasonicSensor1,sim.simx_opmode_blocking);
                 [returnCode,detectionState_2, detectedPoint_2,detectedObjectHandle,detectedSurfaceNormalVector]=sim.simxReadProximitySensor(clientID,ultrasonicSensor2,sim.simx_opmode_blocking);
@@ -113,14 +71,16 @@ function simpleTest()
                 [returnCode,detectionState_6, detectedPoint_6,detectedObjectHandle,detectedSurfaceNormalVector]=sim.simxReadProximitySensor(clientID,ultrasonicSensor6,sim.simx_opmode_blocking);
                 [returnCode,detectionState_7, detectedPoint_7,detectedObjectHandle,detectedSurfaceNormalVector]=sim.simxReadProximitySensor(clientID,ultrasonicSensor7,sim.simx_opmode_blocking);
                 [returnCode,detectionState_8, detectedPoint_8,detectedObjectHandle,detectedSurfaceNormalVector]=sim.simxReadProximitySensor(clientID,ultrasonicSensor8,sim.simx_opmode_blocking);
-                
+                %wektor zawierający stan wykrycia przeszkód na
+                %poszczególnych sensorach
                 detStateVector=[detectionState_1;detectionState_2;detectionState_3;detectionState_4;detectionState_5;detectionState_6; detectionState_7;detectionState_8];
+                %współrzędne wykrytych punktów 
                 detPointMatrix=[detectedPoint_1;detectedPoint_2;detectedPoint_3;detectedPoint_4;detectedPoint_5;detectedPoint_6;detectedPoint_7;detectedPoint_8];
                 
 
                  
-                
-                if CalcDist(r_pose,goal) < 0.1
+                %jeżeli dystans do celu mniejszy niż 10 cm - cel osiagniety - STOP
+                if CalcDist(r_pose,goal) < 0.1 
                     path_completed=1;
                     fprintf('punkt %d osiągnięty! \n', sensorID);
                     [returnCode]= sim.simxSetJointTargetVelocity(clientID, left_Motor, 0 , sim.simx_opmode_blocking);
@@ -130,26 +90,48 @@ function simpleTest()
                 end
                 
                 if path_completed==0
-                %[om_des,w]=PurePursuit2(r_pose(1),r_pose(2),alfa_abs,goal,angle_correction);
-                %[rad2deg(alfa_abs), rad2deg(phi)]
-%                      if sum(detStateVector) ==0
-%                         [om_des, angle_error]=GoalPursuit(r_pose(1),r_pose(2),alfa_abs,goal);
-%                         d=ones(8,1);
-%                      else
-                        [angle_correction,d]=HistogramAngle(detStateVector,detPointMatrix,v_des,150,0.1, 0);
-                        angle_correction
+           
+                     if sum(detStateVector) ==0 % jeżeli nie ma żadnej przeszkody - jedź prosto do celu
+                        [om_des, angle_error,phi]=GoalPursuit2(r_pose(1),r_pose(2),alfa_abs,goal);
+                        
+                        %wykres pomocniczy
+%                         angle_hist=[angle_hist; rad2deg(angle_error)];
+%                         phi_hist=[phi_hist;rad2deg(phi)];
+%                         alpha_hist=[alpha_hist;rad2deg(alfa)];
+                        
+                     else %jeżeli jest jakakolowiek przeszkoda na horyzoncie - zajmij się omijaniem przeszkód
+                        [angle_correction,d]=HistogramAngle(detStateVector,detPointMatrix,v_des,150,0.1, 1);
                         om_des= 0.8*deg2rad(angle_correction);
-                        %v_des=0.05*min(d)+0.05;
+                       
                      end
                 
-%                  end
-             
-                        figure(1)
-                        bar(d)
+                end
                 
-                %om_des=0;
-                r_wheel=0.0975;
-                L=0.331; %sprawdzic dokladny rozstaw kol
+                %przytrzymaj robota przez 0.5 sek nieruchomo i obróć w
+                %kierunku celu
+%                 if reorient <= 5
+%                   v_des=0;
+%                 end
+%                 reorient=reorient+1; %TODO: zerować reorient
+                
+                figure(1) %rysuj histogram
+                 bar(d)
+                %wykres pomocniczy
+%                 subplot(1,2,1)
+%                 plot(angle_hist);
+%                 hold on
+%                 plot(alpha_hist);
+%                 plot(phi_hist);
+%                 legend('alg ang','alfa','phi');
+%                 hold off
+%                 subplot(1,2,2)
+
+
+               %sterowanie napędami robota na podstawie prędkości liniowej
+               %i obrotowej
+      
+                r_wheel=0.0975; %promień koła
+                L=0.331; %rozstaw kół
                 [L_rotvel,R_rotvel]=DiffDrive(v_des,om_des,L,r_wheel);
                 [returnCode]= sim.simxSetJointTargetVelocity(clientID, left_Motor, L_rotvel , sim.simx_opmode_blocking);
                 [returnCode]= sim.simxSetJointTargetVelocity(clientID, right_Motor, R_rotvel , sim.simx_opmode_blocking);
